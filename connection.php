0e17bdfc69c16304ed10f9624dd6550da9449d73
Marlon Juhn M. Timogan <104192810+mjsouthern@users.noreply.github.com>
2022-07-02 20:20:49 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'lamr';
+$servername = 'remotemysql.com';
+$username = 'OrkB5sKFpZ';
+$password = 'cUkePiTeEU';
+$dbname = 'OrkB5sKFpZ';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
