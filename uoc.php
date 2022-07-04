97f2b356f1dc009a6ab51331df8e929c76b94f1d
Marlon Juhn M. Timogan <104192810+mjsouthern@users.noreply.github.com>
2022-07-04 16:53:18 +0800

    Update uoc.php

--- uoc.php
+++ uoc.php
@@ -5,9 +5,12 @@ $data = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>LAMR</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAMR</title>
 </head>
 <style type="text/css">
 	body {
